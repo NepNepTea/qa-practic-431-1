b02e6e76b3fe12a842476dedf0495847e1ca170b
NepNepTea <162781744+NepNepTea@users.noreply.github.com>
2025-05-15 20:35:39 +0700

    small update and autorization tests files

--- cypress/e2e/registration_positive.cy.js
+++ cypress/e2e/registration_positive.cy.js
@@ -3,11 +3,17 @@ describe('template spec', () => {
     cy.visit('https://dev.profteam.su')
     cy.contains('Регистрация').click()
     cy.url().should('include', '/registration')
+
     cy.get('input[placeholder*="Латинские символы"]').type('logintest')
     cy.get('input[type*="email"]').type('test@test.ru')
     cy.get('input[type*="password"]').eq(0).type('Test98')
     cy.get('input[type*="password"]').eq(1).type('Test98')
     cy.contains('Далее').click()
+
+    cy.get('input[placeholder*="Иван"]').eq(0).type('Револьвер')
+    cy.get('input[placeholder*="Иван"]').eq(1).type('Оцелот')
     cy.contains('Создать аккаунт').click()
+
+    cy.url().should('include', '/account/main')
   })
 })
